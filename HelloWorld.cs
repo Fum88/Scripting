0302074a4ef4e98e483e8baffcd826c450220556
Fum88 <demik.37@yandex.ru>
2024-12-19 19:22:08 +0300

    Add files via upload
    first srcrpt

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Печать: Привет мир!
+        print("Hello, word!"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
